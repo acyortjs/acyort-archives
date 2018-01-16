c383ac5899ede833077c377be255ef1fc35f680d
LoeiFy <LoeiFy@gmail.com>
2018-01-16 14:16:00 +0800

    use acyort-pagination

--- test/archives.js
+++ test/archives.js
@@ -1,5 +1,2 @@
-const processor = require('../lib/processor')
-const generator = require('../lib/generator')
-
-acyort.extend.register('after_process', processor.bind(acyort))
-acyort.extend.register('after_build', generator.bind(acyort))
+acyort.extend.register('after_process', require('../lib/processor').bind(acyort))
+acyort.extend.register('after_build', require('../lib/generator').bind(acyort))
