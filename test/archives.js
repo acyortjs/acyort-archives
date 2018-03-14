68c970f70633cfad50e74423ccf47e30746c6f88
LoeiFy <LoeiFy@gmail.com>
2018-03-14 20:31:22 +0800

    filter

--- test/archives.js
+++ test/archives.js
@@ -1,5 +1,4 @@
 const { archives = {} } = acyort.config
 acyort.template.register([archives.template || 'archives'])
-
-acyort.extend.register('after_process', require('../lib/processor').bind(acyort))
-acyort.extend.register('after_build', require('../lib/generator').bind(acyort))
+acyort.filter.register('after_process', require('../lib/processor').bind(acyort))
+acyort.filter.register('after_build', require('../lib/generator').bind(acyort))
