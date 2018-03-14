05cc0d3cf0bc353ad1f5a2cc12f99b759642deb7
LoeiFy <LoeiFy@gmail.com>
2018-03-14 08:40:40 +0800

    template api

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const { archives = {} } = acyort.config
-acyort.builder.register([archives.template || 'archives'])
+acyort.template.register([archives.template || 'archives'])
 
 acyort.extend.register('after_process', require('./lib/processor').bind(acyort))
 acyort.extend.register('after_build', require('./lib/generator').bind(acyort))
